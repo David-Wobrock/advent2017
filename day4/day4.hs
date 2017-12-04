81c7f633be4a542dcae21813e6c1b40713dc8213
David Wobrock <david.wobrock@gmail.com>
2017-12-04 15:40:55 +0100

    Day 4 done in Haskell

---
+++ day4/day4.hs
@@ -0,0 +1,20 @@
+import Data.List.Split(splitOn)
+import Data.Set(fromList, size)
+import System.Environment
+import System.IO
+
+is_valid :: [String] -> Bool
+is_valid line =
+    (length line) == (size (fromList line))
+
+inc_if_valid line =
+  is_valid (splitOn " " line)
+
+main = do
+    args <- getArgs
+    handle <- openFile (head args) ReadMode
+    content <- hGetContents handle
+    let correct = map inc_if_valid (lines content)
+    let num_true = length (filter (== True) correct)
+    print num_true
+    hClose handle
